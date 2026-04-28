eb1ad8ae9c8504c68c71c6cc7de3ff37779be21a
Victor Hugo <hugodk33@gmail.com>
2026-04-28 13:14:16 -0300

    fixing table games scenes

--- src/domain/repositories/GameTableRepository.ts
+++ src/domain/repositories/GameTableRepository.ts
@@ -426,10 +426,10 @@ export class GameTableRepository implements IGameTableRepository {
           name: row.action_name,
           description: row.action_description,
           userId: row.action_user_id,
-          character: row.character_id
+          character: row.action_character_id
             ? {
-              id: row.character_id,
-              name: row.character_name
+              id: row.action_character_id,
+              name: row.action_character_name
             }
             : null
         })
